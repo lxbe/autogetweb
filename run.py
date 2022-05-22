d03088793050d190e9fde28bf48eb58a7ad121e8
lxbe <8username@gmail.com>
2022-05-22 09:12:03 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 try:
     import requests
 except Exception as e:
-    print str(e) + "缺少requests模块"
+    print(str(e) + "缺少requests模块")
 #
 def ggg():
 	url = "https://usvl.coco88.tk"
@@ -9,5 +9,5 @@ def ggg():
 		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36'
 	}
 	res = requests.get(url=url, headers=headers)
-	print "连接状态:" + str(res.status_code)
+	print("连接状态:" + str(res.status_code))
 ggg()
