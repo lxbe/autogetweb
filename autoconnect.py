d6d8c68c417e05fa832ba17231f2ca45c761bea0
lxbe <8username@gmail.com>
2022-05-22 09:01:35 +0800

    Add files via upload

---
+++ autoconnect.py
@@ -0,0 +1,14 @@
+import tostring
+try:
+    import requests
+except Exception as e:
+    print(tostring(e) + "缺少requests模块")
+#
+def ggg():
+	url = "https://usvl.coco88.tk"
+	headers = {
+		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36'
+	}
+	res = requests.get(url=url, headers=headers)
+	print("连接状态:" + tostring(res.status_code))
+ggg()
