da2042739272fc7f4f1176a369c0540b69cd5ced
lxbe <8username@gmail.com>
2022-05-26 18:46:47 +0800

    Update run.py

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ except Exception as e:
     print(str(e) + "缺少requests模块")
 #
 def ggg():
-	url = "https://usvl.coco88.tk"
+	url = "https://usvm.coco88.tk"
 	headers = {
 		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36'
 	}
